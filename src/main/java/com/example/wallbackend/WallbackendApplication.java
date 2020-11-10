89e0ea2e3db20f7cfddf8ae8af9e903f1d8ac8b4
Jovana Grabez <grabez.e25.2019@uns.ac.rs>
2020-11-10 21:27:28 +0100

    initial commit

---
+++ src/main/java/com/example/wallbackend/WallbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.wallbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WallbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WallbackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wallbackend/WallbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wallbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WallbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
